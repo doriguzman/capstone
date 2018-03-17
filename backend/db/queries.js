0650aeb2d69afc97e038a7fb2712afc0a6cc3858
doriguzman <dorianguzman@ac.c4q.nyc>
2018-03-17 19:04:52 -0400

    fixed ports, added modules, and did user registration

--- backend/db/queries.js
+++ backend/db/queries.js
@@ -1,42 +1,44 @@
-//query to get user tables and to check username first
+const pgp = require('pg-promise')({})
+const db = pgp('postgres://localhost/feathers')
+const authHelpers = require('../auth/helpers')
+const passport = require('../auth/local')
 
 // Information on all users
 function getAllUsers(req, res, next) {
-  db
-    .any("SELECT * FROM users")
-    .then(data => {
-      console.log("data:", data);
-      res.status(200).json({
-        status: "success",
-        data: data,
-        message: "Retrieved all users"
-      });
-    })
-    .catch(err => {
-      return next(err);
-    });
+    db.any('SELECT * FROM users')
+        .then((data) => {
+            console.log("data:", data)
+            res.status(200).json({
+                status: 'success',
+                data: data,
+                message: 'Retrieved all users'
+            })
+        })
+        .catch((err) => {
+            return next(err)
+        })
 }
 
 // Registers user using email, username, password, fullname
-function registerUser(req, res, next) {
-  let hash = authHelpers.createHash(req.body.password);
-  db
-    .none(
-      "INSERT INTO users (username, password_digest, email_add, fullname) VALUES ($1, $2, $3, $4)",
-      [req.body.username, hash, req.body.email, req.body.fullname]
-    )
-    .then(() => {
-      res.status(200).json({
-        message: "Registration successful"
-      });
-    })
-    .catch(err => {
-      res.status(500).json({
-        status: "Error",
-        error: err
-      });
-    });
-}
+// function registerUser(req, res, next) {
+//   let hash = authHelpers.createHash(req.body.password);
+//   db
+//     .none(
+//       "INSERT INTO users (username, password_digest, email_add, fullname) VALUES ($1, $2, $3, $4)",
+//       [req.body.username, hash, req.body.email, req.body.fullname]
+//     )
+//     .then(() => {
+//       res.status(200).json({
+//         message: "Registration successful"
+//       });
+//     })
+//     .catch(err => {
+//       res.status(500).json({
+//         status: "Error",
+//         error: err
+//       });
+//     });
+// }
 
 // Get a user's attributes
 function getUserAttributes(req, res, next) {
@@ -60,4 +62,12 @@ function getMatches(req, res, next) {}
 // Log out user
 function logout() {
 	
+}
+
+module.exports = {
+    getAllUsers: getAllUsers,
+    // getSingleUser: getSingleUser,
+    getUserAttributes:getUserAttributes,
+    // registerUser: registerUser,
+    // logoutUser: logoutUser
 }
