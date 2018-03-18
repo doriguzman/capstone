8899b5106d656d4c43acfb943bc59e36d151ca62
Michelle <michelleshang@ac.c4q.nyc>
2018-03-18 14:17:56 -0400

    add gitignore file. queries: add createUser and logoutUser functions.

--- backend/db/queries.js
+++ backend/db/queries.js
@@ -1,44 +1,52 @@
-const pgp = require('pg-promise')({})
-const db = pgp('postgres://localhost/feathers')
-const authHelpers = require('../auth/helpers')
-const passport = require('../auth/local')
+const pgp = require("pg-promise")({});
+const db = pgp("postgres://localhost/feathers");
+const authHelpers = require("../auth/helpers");
+const passport = require("../auth/local");
+
+// Create a new user
+function createUser(req, res, next) {
+  const hash = authHelpers.createHash(req.body.password);
+  // console.log("create user hash: ", hash);
+  db
+    .none(
+      "INSERT INTO users (username, first_name, password_digest, age, email, my_location, bio, pic, ethnicity) VALUES (${username}, ${firstName}, ${password} ${age}, ${email}, ${location}, ${bio}, ${pic}, ${ethnicity})",
+      {
+        username: req.body.username,
+        firstName: req.body.firstName,
+        password: hash,
+        age: req.body.age,
+        email: req.body.email,
+        location: req.body.location,
+        bio: req.body.bio,
+        pic: req.body.pic,
+        ethnicity: req.body.ethnicity
+      }
+    )
+    .then(() => {
+      res.send(`created user: ${req.body.username}`);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send("error creating user");
+    });
+}
 
 // Information on all users
 function getAllUsers(req, res, next) {
-    db.any('SELECT * FROM users')
-        .then((data) => {
-            console.log("data:", data)
-            res.status(200).json({
-                status: 'success',
-                data: data,
-                message: 'Retrieved all users'
-            })
-        })
-        .catch((err) => {
-            return next(err)
-        })
+  db
+    .any("SELECT * FROM users")
+    .then(data => {
+      console.log("data:", data);
+      res.status(200).json({
+        status: "success",
+        data: data,
+        message: "Retrieved all users"
+      });
+    })
+    .catch(err => {
+      return next(err);
+    });
 }
-
-// Registers user using email, username, password, fullname
-// function registerUser(req, res, next) {
-//   let hash = authHelpers.createHash(req.body.password);
-//   db
-//     .none(
-//       "INSERT INTO users (username, password_digest, email_add, fullname) VALUES ($1, $2, $3, $4)",
-//       [req.body.username, hash, req.body.email, req.body.fullname]
-//     )
-//     .then(() => {
-//       res.status(200).json({
-//         message: "Registration successful"
-//       });
-//     })
-//     .catch(err => {
-//       res.status(500).json({
-//         status: "Error",
-//         error: err
-//       });
-//     });
-// }
 
 // Get a user's attributes
 function getUserAttributes(req, res, next) {
@@ -56,18 +64,21 @@ function getUserAttributes(req, res, next) {
 }
 
 // get matches by attributes
-function getMatches(req, res, next) {}
-
+function getMatches(req, res, next) {
+  db.any();
+}
 
 // Log out user
-function logout() {
-	
+function logoutUser(req, res, next) {
+  req.logout();
+  res.status(200).send("log out success");
 }
 
 module.exports = {
-    getAllUsers: getAllUsers,
-    // getSingleUser: getSingleUser,
-    getUserAttributes:getUserAttributes,
-    // registerUser: registerUser,
-    // logoutUser: logoutUser
-}
+	createUser: createUser,
+  getAllUsers: getAllUsers,
+  // getSingleUser: getSingleUser,
+  getUserAttributes: getUserAttributes,
+  // registerUser: registerUser,
+  logoutUser: logoutUser
+};
