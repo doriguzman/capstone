e2b84b0f28c98019b9d49820754d48f2e552481f
Michelle <michelleshang@ac.c4q.nyc>
2018-03-22 11:45:26 -0400

    made some changes to feed. it's not working yet.

--- front-end/src/LoggedInUser/FEED/AllBuddies.jsx
+++ front-end/src/LoggedInUser/FEED/AllBuddies.jsx
@@ -1,20 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
+class AllBuddies extends Component {
+  constructor() {
+    super();
+    this.state = {
+			user: null,
+			allUsers: [],
+			errorMsg: ""
+    };
+  }
 
-class AllBuddies extends React.Component{
-    constructor(){
-        super();
-        this.state={
-            user:
-        }
-    }
-    render(){
-        return(
-            <div>
-                </div> 
-        )
-    }
+  componentWillMount() {
+		axios
+			.get("/users/")
+			.then(response => {
+				console.log(response)
+				let usernames = response.data.data.map(userObj => userObj.username)
+
+				this.setState({
+					allUsers: usernames
+				})
+			})
+			.catch(err => {
+				console.log(err)
+				this.setState({
+					errorMsg: "Sorry, there's something wrong with your feed."
+				})
+			});
+	}
+	
+	componentDidMount() {
+		
+	}
+
+  render() {
+		console.log(this.state.allUsers)
+    return <div />;
+  }
 }
-
 
 export default AllBuddies;
